0e281284357825b627ab439ce65bea7a0eac8778
Adnan BOUDJELAL <aboudjel@e1r8p17.clusters.42paris.fr>
2022-09-05 00:55:32 +0200

    juste au cas ou

--- cube3d/sources/cub3d.c
+++ cube3d/sources/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: aboudjel <aboudjel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 17:41:46 by aboudjel          #+#    #+#             */
-/*   Updated: 2022/09/04 10:37:06 by aboudjel         ###   ########.fr       */
+/*   Updated: 2022/09/05 00:53:31 by aboudjel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,31 +26,76 @@ float conv_rad(float angle)
 	return (modulo(0- modulo(angle,(2 * PI)), 2 * PI));
 }
 
-// int	ft_raycasting(t_global *data)
-// {
-// 	int r,mx, my, mp, dof;
-// 	float rx, ry, ra, xo, yo;
+int	ft_raycasting_vertical(t_global *data, int col2, int row2)
+{
+	float adj_y;
+	float distance;
+	float	col_x;
+	float	col_y;
 
-// 	ra = data->player.angle;
-// 	for (int r = 0; r < 1; r++)
-// 	{a
-// 		dof = 0;
-// 		float aTan = -1 / tan(ra);
-// 		if (ra < PI){
-// 			ry = (((int)(data->player.x / 64) >> 6) << 6) - 0.0001;
-// 			rx = (data->player.y - )
-// 		}
-// 	}
-// }
+	if (conv_rad(data->player.angle) > 0 && conv_rad(data->player.angle) < PI) //regarde a droite
+	{
+		adj_y = 64 - modulo(data->player.y, 64); printf("Aaaaaaaaaaaaaaaa\n");
+	}
+	else //  ((data->player.angle > (PI / 2)) && (data->player.angle < (2 * PI))) //regarde a gauche
+	{
+		adj_y = - modulo(data->player.y, 64); printf("Bbbbbbbbbbbbbbbbbbbb\n");
+	}
+	printf("coord : x : %f\t y : %f\n", data->player.x, data->player.y);
+	printf("adj : %f\nangle conv: %f\nangle \t: %f\n", adj_y, conv_rad(data->player.angle), data->player.angle);
+	// while (1)
+	// {
+		col_y = data->player.y + adj_y;
+		distance = adj_y / cos(data->player.angle);
+		put_pixel_to_frame_buf(data, col2 + data->player.x + (cos(conv_rad(data->player.angle))),
+						row2 + data->player.y + adj_y + (sin(conv_rad(data->player.angle))), 0xFFC0CB);
+		if (distance < 0)
+			distance = -distance;
+		printf("adj = %f hypotheus : %f\n", adj_y, distance);
 
-int	ft_raycasting(t_global *data, int col2, int row2)
+		
+		if ((data->player.angle < (PI / 2)) || (data->player.angle > (PI + (PI / 2))))
+			col_x = data->player.x + (sqrt(pow(distance, 2) - pow(adj_y, 2))); 
+		else // else if (data->player.angle >= PI && data->player.angle <= PI*2)
+			col_x = data->player.x - (sqrt(pow(distance, 2) - pow(adj_y, 2)));
+		
+		if (col_x < 0 || col_x / 64 > data->map.largeur) //|| col_x < 0 || col_x / 64 > data->map.largeur)
+			return(0);
+		
+		put_pixel_to_frame_buf(data, col2 + col_x + (cos(conv_rad(data->player.angle))),
+						row2 + col_y + (sin(conv_rad(data->player.angle))), 0xFFC0CB);
+		// if ((data->player.angle < (PI / 2)) || (data->player.angle > (PI + (PI / 2)))) //regarde a droite
+		// {
+	// 		// if (data->map.map[(int)round(col_y)][(int)round(col_x) + 32] == '1')
+	// 			// break;
+	// 		// else
+				// adj_y += 64;
+		// }
+		// else
+		// {
+	// 		// if (data->map.map[(int)round(col_y)][(int)round(col_x) - 32] == '1')
+	// 			// break;
+	// 		// else
+				// adj_y -= 64;
+		// }
+	// // }
+	// for (float i = 0; i < distance; i += 0.1f)
+	// 			put_pixel_to_frame_buf(data, col2 + col_x - (cos(i)),
+	// 							row2 + data->player.y - (sin(i)), 0xBDB67A);
+	// for (int i = 0; i < distance; i++)
+	// 	put_pixel_to_frame_buf(data, col2 + data->player.x + (cos(conv_rad(data->player.angle)) * i),
+	// 					row2 + data->player.y + (sin(conv_rad(data->player.angle))* i), 0xFFC0CB);
+	return (0);
+}
+
+
+int	ft_raycasting_horizontal(t_global *data, int col2, int row2)
 {
 	float adj_x;
 	float distance;
 	float	col_x;
 	float	col_y;
-	// (void)col2;
-	// (void)row2;
+
 	if ((data->player.angle < (PI / 2)) || (data->player.angle > (PI + (PI / 2)))) //regarde a droite
 	{
 		adj_x = 64 - modulo(data->player.x, 64); printf("Aaaaaaaaaaaaaaaa\n");
@@ -76,8 +121,7 @@ int	ft_raycasting(t_global *data, int col2, int row2)
 		
 		if (col_y < 0 || col_y / 64 > data->map.hauteur) //|| col_x < 0 || col_x / 64 > data->map.largeur)
 			return(0);
-		
-
+			
 		if ((data->player.angle < (PI / 2)) || (data->player.angle > (PI + (PI / 2)))) //regarde a droite
 		{
 			if (data->map.map[(int)round(col_y)][(int)round(col_x) + 32] == '1')
@@ -102,59 +146,6 @@ int	ft_raycasting(t_global *data, int col2, int row2)
 	return (0);
 }
 
-// int ft_raycasting(t_global *data, int col2, int row2)
-// {
-// 	float adj_x;
-// 	float distance;
-// 	float	col_x;
-// 	float	col_y;
-
-// 	if ((data->player.angle < (PI / 2)) || (data->player.angle > (PI + (PI / 2)))) //regarde a droite
-// 	{
-// 		adj_x = - modulo(data->player.x, 64); printf("Bbbbbbbbbbbbbbbbbbbb\n");
-// 	}
-// 	else //  ((data->player.angle > (PI / 2)) && (data->player.angle < (2 * PI))) //regarde a gauche
-// 	{
-// 		adj_x = 64 - modulo(data->player.x, 64); printf("Aaaaaaaaaaaaaaaa\n");
-
-// 	}
-// 	// else
-// 	// 	return (1); //a voir
-// 	while (1)
-// 	{	
-// 		printf("adj : %f\nangle conv: %f\nangle \t: %f\n", adj_x, conv_rad(data->player.angle), data->player.angle);
-// 		printf("adj = %f\n", adj_x);
-// 		distance = adj_x / cos(data->player.angle);
-// 		if (distance < 0)
-// 			distance = -distance;
-// 		printf("adj = %f hypotheus : %f\n", adj_x, distance);
-// 		col_x = data->player.x + adj_x;
-// 		if (data->player.angle > 0 && data->player.angle < PI)
-// 			col_y = data->player.y + (sqrt(pow(distance, 2) - pow(adj_x, 2))); 
-// 		else // else if (data->player.angle >= PI && data->player.angle <= PI*2)
-// 			col_y = data->player.y - (sqrt(pow(distance, 2) - pow(adj_x, 2)));
-// 		printf("%f %f\n", col_x, round(col_x));
-// 		if ((data->player.angle < (PI / 2)) || (data->player.angle > (PI + (PI / 2)))) //regarde a droite
-// 		{
-// 			if(data->map.map[(int)round(col_y)][(int)round(col_x) + 32] == '1')
-// 				break;
-// 		}
-// 		else if (data->player.angle > PI / 2 && data->player.angle < 2 * PI / 3) //regarde a gauche)
-// 		{
-// 			if(data->map.map[(int)round(col_y)][(int)round(col_x) - 32] == '1')
-// 				break;
-// 		}
-// 		if (data->player.angle < PI / 2 || data->player.angle > 2 * PI / 3) //regarde a droite
-// 			adj_x += 64;
-// 		else
-// 			adj_x -= 64;
-// 	}
-// 	for (float i = 0; i < distance; i += 0.1f)
-// 		put_pixel_to_frame_buf(data, col2 + data->player.x - (cos(data->player.angle) * i),
-// 						row2 + data->player.y - (sin(data->player.angle)* i), 0xFFC0CB);
-// 	return (0);
-// }
-
 int	ft_screen(t_global *data)
 {
 	int		row;
@@ -173,7 +164,10 @@ int	ft_screen(t_global *data)
 		while (data->map.map[row][col])
 		{
 			// printf("%s\n", data->map.map[row]);
-			if (data->map.map[row][col] == '1')
+			if (row % 64 == 0 || col % 64 == 0)
+				put_pixel_to_frame_buf(data, (col2 + col),
+						 (row2 + row), 0x606060);
+			else if (data->map.map[row][col] == '1')
 				put_pixel_to_frame_buf(data, (col2 + col),
 						 (row2 + row), 0x0000FF);
 			else if (data->map.map[row][col] == '0')
@@ -199,7 +193,8 @@ int	ft_screen(t_global *data)
 	for (int i = 0; i < 30; i++)
 		put_pixel_to_frame_buf(data, col2 + data->player.x + (cos(conv_rad(data->player.angle)) * i),
 						row2 + data->player.y + (sin(conv_rad(data->player.angle))* i), 0xFFC0CB);
-	ft_raycasting(data, col2, row2);
+	ft_raycasting_vertical(data, col2, row2);
+	ft_raycasting_horizontal(data, col2, row2);
 	// ft_image_to_frame(data, data->player.img, data->player.row,
 	// 	data->player.col);
 	// ft_moves(data);ssssss
